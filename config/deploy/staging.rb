2d65940d13b1474ccd67cdd3bb1ddd976ebc7d37
minimallinux <anthonymcgowan1@gmail.com>
2017-11-19 17:38:22 +0000

    Update

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -14,8 +14,8 @@ server 'vhost1.com', user: 'deploy', roles: %w{web app db}
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(~/.ssh/id_rsa),
+  set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
